385f48d04f9e5c0378d6b52750ca9d30a59ae56f
Hamit Taylan YUCE <h.taylanyuce@gmail.com>
2018-09-01 10:05:12 +0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <h1>My name is Hamit Taylan Yuce from Turkey </h1>
+    <h2>I have telecommunications engineering background but am interested in SW development.</h2>
+
+  </div>
+)
+
+export default IndexPage
